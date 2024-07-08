705c9c8c8f4c6c0013054bd1089fbf9b11aff07d
avishka <avishkasamarawickrama@gmail.com>
2024-07-08 11:46:37 +0530

    BO  has been created

--- src/main/java/lk/ijse/pos/controller/salaryFormController.java
+++ src/main/java/lk/ijse/pos/controller/salaryFormController.java
@@ -1,8 +1,6 @@
 package lk.ijse.pos.controller;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
