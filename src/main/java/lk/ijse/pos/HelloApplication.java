99472d9a692bf49125dae4e04111004b96c6fa5e
avishka <avishkasamarawickrama@gmail.com>
2024-07-11 09:41:33 +0530

    updated

--- src/main/java/lk/ijse/pos/HelloApplication.java
+++ src/main/java/lk/ijse/pos/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/lk/ijse/pos/CustomerForm.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/lk/ijse/pos/dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("LOGIN");
         stage.setScene(scene);
